4b9fe262298b31ef060eeb9add7022370b0f33fe
Dmytro Ivasenko <dmtivasenko@gmail.com>
2024-08-31 20:57:25 +0700

    make build

---
+++ index.cb07cae7.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider");t.style.top=e.clientHeight/2+"px",t.style.left=e.clientHeight/2+"px",t.style.transform="translate(-50%, -50%)";
+//# sourceMappingURL=index.cb07cae7.js.map
